ec3d71714931908333f55d35be12043dd8e39ae8
MartinelliNTZ <martinelli.matheus0@gmail.com>
2022-11-14 00:17:30 -0300

    fim aula

--- Gato.java
+++ Gato.java
@@ -1,8 +1,10 @@
+import java.util.Comparator;
+
 /**
  * @author Matheus Martinelli
  * 13-11-2022
  */
-public class Gato {
+public class Gato implements Comparable<Gato> {
     private String nome;
     private Integer idade;
     private String cor;
@@ -40,5 +42,44 @@ public class Gato {
     @Override
     public String toString() {
         return "Gato{" + "nome='" + nome + '\'' + ", idade=" + idade + ", cor='" + cor + '\'' + '}';
+    }
+
+    /**
+     * Se os nome forem iguais retorna 0
+     * se o gato tiver que ser adicionado na frente na fila retorna 1
+     * se o gato tiver que ser adicionado atras na fila retorna -1*/
+    @Override
+    public int compareTo(Gato gato) {
+        return this.nome.compareToIgnoreCase(gato.getNome());
+    }
+}
+
+class compareIdadeGato implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return Integer.compare(g1.getIdade(), g2.getIdade());
+    }
+}
+
+class compareCorGato implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        return g1.getCor().compareToIgnoreCase(g2.getCor());
+    }
+}
+
+class compareNomeCorIdadeGato implements Comparator<Gato> {
+
+    @Override
+    public int compare(Gato g1, Gato g2) {
+        int nome = g1.getNome().compareToIgnoreCase(g2.getNome());
+        if (nome != 0) return nome;
+
+        int cor = g1.getCor().compareToIgnoreCase(g2.getCor());
+        if (cor != 0) return cor;
+
+        return Integer.compare(g1.getIdade(), g2.getIdade());
     }
 }
--- OrdenarLists.java
+++ OrdenarLists.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,9 +14,40 @@ public class OrdenarLists {
     }
 
     private static void f() {
-        List<Gato> cats = new ArrayList<>();
-        cats.add(new Gato("Jon", 18, "preto"));
-        cats.add(new Gato("Simba", 6, "tigrado"));
-        cats.add(new Gato("Jon", 12, "amarelo"));
+        List<Gato> cats = new ArrayList<>() {
+            {
+                add(new Gato("Jon", 18, "preto"));
+                add(new Gato("Simba", 6, "tigrado"));
+                add(new Gato("Jon", 12, "amarelo"));
+                add(new Gato("Jon", 15, "amarelo"));
+                add(new Gato("Jon", 15, "preto"));
+                add(new Gato("Poro", 5, "Rosa"));
+                add(new Gato("Libano", 11, "Rosa"));
+                add(new Gato("Libano", 15, "Rosa"));
+            }
+        };
+        System.out.println("\n\t Ordem de irserção!!");
+        System.out.println(cats);
+
+        System.out.println("\n\t---\t Aleatório!! \t---");
+        Collections.shuffle(cats);
+        System.out.println(cats);
+        // a ordem ira ocorrer de acordo com o metodo compareto
+        System.out.println("\n\t---\t Ordem Natural(Nome)!! \t---");
+        Collections.sort(cats);
+        System.out.println(cats);
+
+        System.out.println("\n\t---\t Ordem Idade()!! \t---");
+        // Collections.sort(cats, new compareIdadeGato());
+        cats.sort(new compareIdadeGato());
+        System.out.println(cats);
+
+        System.out.println("\n\t---\tOrdenado por Cor!! \t---");
+        cats.sort(new compareCorGato());
+        System.out.println(cats);
+
+        System.out.println("\n\t---\tOrdenado por Nome Cor Idade!! \t---");
+        cats.sort(new compareNomeCorIdadeGato());
+        System.out.println(cats);
     }
-    }
+}
